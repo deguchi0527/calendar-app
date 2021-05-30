d6f156f1b4e782dc33fda33d58f9793cd720713d
deguchi0527 <ryodegu0527@gmail.com>
2021-05-30 20:55:58 +0900

    ビューの編集

--- app/javascript/event.js
+++ app/javascript/event.js
@@ -17,9 +17,26 @@ $(function() {
     $('.active').removeClass('active');
     // 現在表示されているイベント日を空にする
     $('.event-lists-day').text('');
+    // 表示されている文章(イベントがありません)を削除する
+    $('.event-none').remove();
     // クリックされた日付を表示する
     $('.event-lists-day').prepend(eventDay);
+
     // クリックさてたイベントを表示する
-    $($class).addClass('active');
+    if ($('.event-list').hasClass(set)) {
+      $($class).addClass('active');
+    } else {
+      $('.event-lists').append('<p class="event-none">イベントがありません</p>');
+    }
+  });
+
+  // イベントをホバーすると背景色が変更する
+  $('.event-list').on({
+    'mouseenter': function() {
+      $(this).css('background-color', '#eee');
+    },
+    'mouseleave': function() {
+      $(this).css('background-color', '#fff');
+    }
   });
 });
